1b19fd59a12bba84221af5e73341cef69855d031
savoryboi <andbjerk@yahoo.com>
2022-08-07 11:50:21 -0500

    front folder added

--- controllers/view_routes.js
+++ controllers/view_routes.js
@@ -1,5 +1,14 @@
+const { resolveSoa } = require('dns');
 const db = require('../config/connection')
 
 db.get('/', (req, res) => {
     res.render('index')
+});
+
+db.get('/login', (req, res) =>{
+    res.render('login')
+});
+
+db.get('/register', (req, res) => {
+    res.render('register')
 })
