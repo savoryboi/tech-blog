8b3f6abcbf1cf6f810203e8fa0e81c77e0d5604f
savoryboi <andbjerk@yahoo.com>
2022-08-07 12:55:28 -0500

    nodemon install

--- server.js
+++ server.js
@@ -19,3 +19,8 @@ app.set("view engine", "hbs");
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}!`)
+});
